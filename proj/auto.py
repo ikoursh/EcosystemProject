24cca92b7e0158c0f1734eeb62c3fbba92ffc978
ikoursh <inbar.koursh@gmail.com>
2020-07-11 19:14:49 +0300

    fixed data point freq in auto.py

--- proj/auto.py
+++ proj/auto.py
@@ -1,5 +1,6 @@
 from model2 import Sim
 import argparse
+import math
 
 parser = argparse.ArgumentParser(description='Run the default simulation')
 parser.add_argument("-s", type=int, dest="steps",
@@ -10,8 +11,14 @@ parser.add_argument("-a", help="enable animation", dest="animate", action='store
 args = parser.parse_args()
 
 ms = Sim()
+
+# because this model will only run once, we can maximise the amount of data points without exceeding the maximum of 18277
+# total_data_points = steps/data_point_freq
+data_point_freq = math.floor(args.steps / 18277) - 1
+if data_point_freq == -1:
+    data_point_freq = 1
 if args.animate:
-    ms.animate(args.steps)
+    ms.animate(args.steps, data_point_freq=data_point_freq)
 else:
-    ms.run(args.steps, max_attempts=-1)
+    ms.run(args.steps, max_attempts=-1, data_point_freq=data_point_freq)
 ms.graph(info="v3")
