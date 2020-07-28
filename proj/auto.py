7a16a0a2b39e7a06e31bb62a744c1f226e386dd0
ikoursh <inbar.koursh@gmail.com>
2020-07-28 22:21:39 +0300

    Added data point option for auto.py instead of automatically deferring to maximum

--- proj/auto.py
+++ proj/auto.py
@@ -9,17 +9,21 @@ parser.add_argument("-s", type=int, dest="steps",
 parser.add_argument("-a", help="enable animation", dest="animate", action='store_true')
 parser.add_argument("-v", help="enable verbose", dest="v", action='store_true')
 
+parser.add_argument("-d", type=int, dest="dp",
+                    help='Maximum number of data points, defaults to excel maximum (16383)', default=16383)
+
 args = parser.parse_args()
 
 ms = Sim()
 
 # because this model will only run once, we can maximise the amount of data points without exceeding the maximum of 18277
 # total_data_points = steps/data_point_freq
-data_point_freq = math.floor(args.steps / 16383) + 1
+data_point_freq = math.floor(args.steps / args.dp)
+data_point_freq += 1 if data_point_freq == 0 else 0
 
 if args.v:
     print("data point frequency selected {}".format(data_point_freq))
-    print("expected data points: {}".format(args.steps/data_point_freq))
+    print("expected data points: {}".format(args.steps / data_point_freq))
 
 if args.animate:
     ms.animate(args.steps, data_point_freq=data_point_freq)
